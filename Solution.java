d7f792e4770bb68c39d154e196ea7664947acd5f
Giupro17 <gproietti07@icloud.com>
2025-04-25 09:51:22 -0400

    Question 24 fix

--- Solution.java
+++ Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * Write a description of class Solution here.
@@ -19,33 +20,47 @@ public class Solution
         x = 0;
         
         
-        /*ArrayList temperatures = new ArrayList<Double>();
+        ArrayList<Double> temperatures = new ArrayList<Double>();
         temperatures.add(34.5);
         temperatures.add(39.5);
-        temperatures.add()
-        temperatures.add()
-        temperatures.add()
-        temperatures.add()
-        */
+        temperatures.add(44.5);
+        temperatures.add(24.5);
+        temperatures.add(44.5);
+        temperatures.add(38.5);
+        
+        feverTest(temperatures);
     }
-
+    //quesation 25
+    public void feverTest(ArrayList<Double> temperatures){
+        int count = 0;
+        double max = 0;
+        System.out.println("Number of fever cases reported:" + count);
+        System.out.println("Higher fever case:" + max);
+        for (int i = 0; i < temperatures.size(); i++) {        
+            System.out.println("is fever");
+        }
+        }
+    
+    
+    
+    
     /**
      * An example of a method - replace this comment with your own
      *
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public boolean sampleMethod()
+    public void sampleMethod()
     {
        //question 24
        String c = "car";
        String b = "bus";
-       if (c == b){
-           return true;
+       if (c.equals(b)){
+           System.out.println("they are equal");
            
        }
        else {
-           return false;
+           System.out.println("they are not equal");;
         }
       
     }
